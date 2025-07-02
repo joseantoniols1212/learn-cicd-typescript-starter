89ba9686daf118d512fbfa45f4d4bb756f3d11ca
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2025-07-02 16:51:33 +0200

    linting

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { middlewareAuth } from "./api/middleware.js";
 import { handlerReadiness } from "./api/readiness.js";
 import { handlerNotesCreate, handlerNotesGet } from "./api/notes.js";
 import { handlerUsersCreate, handlerUsersGet } from "./api/users.js";
+import process from "process";
 
 const __dirname = path.resolve();
 
@@ -47,3 +48,9 @@ app.use("/v1", v1Router);
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
+
+
+function unused() {
+  // this function does nothing
+  // and is called nowhere
+}
